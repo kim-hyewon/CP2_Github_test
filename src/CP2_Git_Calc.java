e55031a61afc893cf0aefcd74013b7918a8769a5
kim-hyewon <qwqw7420@naver.com>
2018-10-16 02:30:56 +0900

    test1

--- src/CP2_Git_Calc.java
+++ src/CP2_Git_Calc.java
@@ -11,12 +11,12 @@ public class CP2_Git_Calc {
 }
 
 class Calc {
-	public int add(double a, double b) {
-		return -1;
+	public double add(double a, double b) {
+		return a + b;
 	}
 	
-	public int sub(double a, double b) {
-		return -1;
+	public double sub(double a, double b) {
+		return a-b;
 	}
 	
 	public int mul(double a, double b) {
